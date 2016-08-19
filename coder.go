f3b7c8dd93526dc0fa28e1b5f93d01a83842015f
Martin Lindhe <martin@ubique.se>
2016-08-19 12:22:11 +0200

    base91: update to match changes in github.com/bproctor/base91

--- coder.go
+++ coder.go
@@ -171,11 +171,14 @@ func decodeBase64(src []byte) ([]byte, error) {
 }
 
 func encodeBase91(src []byte) ([]byte, error) {
-	return []byte(base91.Encode(string(src))), nil
+	return []byte(base91.Encode(src)), nil
 }
 
 func decodeBase91(src []byte) ([]byte, error) {
-	return []byte(base91.Decode(string(src))), nil
+	if len(src) == 0 {
+		return []byte{}, nil
+	}
+	return []byte(base91.Decode(src)), nil
 }
 
 func encodeBinary(src []byte) ([]byte, error) {
